cb13b2626d9b6023dffd3dc7fbfa4778f9efc54d
carltonf <xiongc05@gmail.com>
2014-03-22 17:57:16 +0800

    Desktop Switching tool to be used with xbindkeys.
    This script features cyclic switching.

---
+++ desktop_switch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SWITCH_CMD="wmctrl -s"
+
+desk_num=$(wmctrl -d | wc -l)
+current_desk=$(wmctrl -d | egrep '^[0-9]+\s+\*' | awk '{print $1}')
+echo "Total desktop number: [$desk_num]. The current desktop is [$current_desk]."
+
+# function to get mod 3 positive result
+
+cmd="$1"
+case "$cmd" in
+    "next")
+        current_desk=$(((($current_desk + 1) % $desk_num)))
+        echo "Switching to next desktop: [$current_desk]"
+        ;;
+    "prev")
+        # minus 1 mod 3 is the same as plus 2 mod 3
+        current_desk=$(((($current_desk + 2) % $desk_num)))
+        echo "Switching to previous desktop: [$current_desk]"
+        ;;
+    [0-9])                      # only deal with single digit one
+        current_desk=$cmd
+        echo "Switching to numbered desktop: [$current_desk]"
+        ;;
+    *)
+        echo "Usage: $0 <next|prev|[numbers]>"
+        exit 1
+        ;;
+esac
+
+$SWITCH_CMD $current_desk
